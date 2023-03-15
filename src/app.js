b0d3bb986e75fd57e5dd2ab54b2388a0a65fce90
unknown <angeleesc2@gmail.com>
2023-03-15 18:36:40 -0400

    configuracion del cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from "express"
 const app = express();
+import cors from "cors";
 
-
-
+app.use(cors())
 app.get("/", (req, res)=>{
     res.send({
         message:"hola soy el servidor de wolfy"
