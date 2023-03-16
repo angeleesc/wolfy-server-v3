40b59e4e5516d64dc3907ebce4f36a2219cddaff
unknown <angeleesc2@gmail.com>
2023-03-16 14:27:56 -0400

    validacion con middleware

--- src/routes/ipfs.routes.js
+++ src/routes/ipfs.routes.js
@@ -1,10 +1,29 @@
 import express from "express";
 const router = express.Router()
 
-router.get("/",(req, res)=>{
-res.send({
-    message:"estas en a ruta de subida de archivo"
+router.get("/", (req, res) => {
+    res.send({
+        message: "estas en a ruta de subida de archivo"
+    })
 })
+
+router.post("/", (req, res, next) => {
+
+    const errorObjet = {}
+    const validateSchema = {
+
+    }
+
+    next()
+
+}, (req, res) => {
+
+
+    console.log(req.files)
+
+    res.status(200).json({
+        message: "archivo subido"
+    })
 })
 
 export default router;
