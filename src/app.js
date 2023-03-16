644a07a2352f73b4fd96ac582362527ac6141f89
unknown <angeleesc2@gmail.com>
2023-03-16 13:08:20 -0400

    configuracion de la lectura de achivo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from "express"
 const app = express();
 import cors from "cors";
+import fileUpload from "express-fileupload";
 
 app.use(cors())
+app.use(express.json());
+app.use(
+    fileUpload({
+      useTempFiles : true,
+      tempFileDir : '/tmp/'
+    })
+  );
+  
+
+
 app.get("/", (req, res)=>{
     res.send({
         message:"hola soy el servidor de wolfy"
