5317d376a78c86e36ba1d6f357e474f2ea8946ce
unknown <angeleesc2@gmail.com>
2023-04-19 13:27:01 -0400

    correcion caso cuando la nft es un campo vacio

--- src/controllers/alchemiController.js
+++ src/controllers/alchemiController.js
@@ -56,57 +56,84 @@ export async function getNftsByWallet(req, res) {
     // console.log(nfts)
     // console.log("---------------")
 
-    const nftsFormated = nfts.ownedNfts.map((nfts) => {
 
-        // console.log(nfts.contract)
-        // console.log(nfts)
+
+    const nftsFormated = []
+
+    nfts.ownedNfts.forEach((nfts) => {
+
+        if (nfts.media && nfts.media.length > 0) {
+            // console.log("titulos")
+            // console.log(nfts.title)
+            // console.log("tiene media")
+            // console.log(nfts)
+
+            const dataToSent = {
+                collection: nfts.contract.address,
+                collectionType: nfts.contract.tokenType,
+                tokenId: nfts.tokenId,
+                rawMetadata: nfts.tokenUri,
+                thumbnails: nfts.media.map((media) => {
+                    return {
+                        url: media.thumbnail ? media.thumbnail : media.gateway,
+                        format: media.format
+                    }
+                }),
+                nftName: nfts.title,
+                nftType: nfts.tokenType,
+                chainId: chainId.toString(),
+                blockChainNetowork
+
+            }
+
+            // console.log(nfts.title)
+            // console.log(dataToSent.thumbnails)
+
+            nftsFormated.push(dataToSent)
+
+            // console.log(dataToSent.thumbnails)
+
+        } else {
+            console.log("no tiene media")
+        }
 
         // console.log(nfts.title)
-        // console.log(nfts.media[0].thumbnail)
-        // if(nfts.media[0].thumbnail){
+        // // console.log(nfts.media[0])
+
+        // if (nfts.media[0].thumbnail) {
         //     console.log("tiene miniatura")
-        // }else{
+        // } else {
         //     console.log("no tiene miniatura")
         // }
-        // console.log(nfts.media[0])
+        // if (nfts.media[0].gateway) {
+        //     console.log("tiene enlace")
+        // } else {
+        //     console.log("no tiene enlace")
+        // }
 
-        console.log(nfts.media[0].thumbnail)
 
-        console.log(nfts.title)
-        // console.log(nfts.media[0])
 
-        if (nfts.media[0].thumbnail) {
-            console.log("tiene miniatura")
-        } else {
-            console.log("no tiene miniatura")
-        }
-        if (nfts.media[0].gateway) {
-            console.log("tiene enlace")
-        } else {
-            console.log("no tiene enlace")
-        }
+        // const dataToSent = {
+        //     collection: nfts.contract.address,
+        //     collectionType: nfts.contract.tokenType,
+        //     tokenId: nfts.tokenId,
+        //     rawMetadata: nfts.tokenUri,
+        //     thumbnails: nfts.media.map((media) => {
+        //         return {
+        //             url: media.thumbnail ? media.thumbnail : media.gateway,
+        //             format: media.format
+        //         }
+        //     }),
+        //     nftName: nfts.title,
+        //     nftType: nfts.tokenType,
+        //     chainId: chainId.toString(),
+        //     blockChainNetowork
 
-        const dataToSent = {
-            collection: nfts.contract.address,
-            collectionType: nfts.contract.tokenType,
-            tokenId: nfts.tokenId,
-            rawMetadata: nfts.tokenUri,
-            thumbnails: nfts.media.map((media) => {
-                return {
-                    url: media.thumbnail ? media.thumbnail : media.gateway,
-                    format: media.format
-                }
-            }),
-            nftName: nfts.title,
-            nftType: nfts.tokenType,
-            chainId: chainId.toString(),
-            blockChainNetowork
+        // }
 
-        }
+        // console.log(dataToSent.thumbnails)
 
-        console.log(dataToSent.thumbnails)
-
-        return dataToSent
+        // return dataToSent
     })
 
     // console.log(nftsFormated)
