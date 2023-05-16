d82bfcafe5f1d5747679e8558ae33b90ae17ca18
unknown <angeleesc2@gmail.com>
2023-05-16 14:47:56 -0400

    validavion de los campos faltantes

--- src/routes/alchemy.rotes.js
+++ src/routes/alchemy.rotes.js
@@ -3,7 +3,6 @@ const routes = express.Router()
 import { getFullNftData, getNftsByWallet } from "../controllers/alchemiController.js";
 
 routes.get("/", getNftsByWallet)
-
 routes.get("/nft", getFullNftData)
 
 export default routes
